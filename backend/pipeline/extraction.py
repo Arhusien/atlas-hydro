fba9c08ac053f898950a37d6da70f5515a5339ae
Alexis <87914117+Arhusien@users.noreply.github.com>
2026-04-01 22:52:28 -0400

    Extraire les relevés par batch de 3 heures

--- backend/pipeline/extraction.py
+++ backend/pipeline/extraction.py
@@ -23,31 +23,25 @@ JEUX_DONNEES = [
 FUSEAU_HORAIRE = ZoneInfo(constantes.FUSEAU_HORAIRE)
 
 
-def _construire_filtre_date(cle_date: str, date_limite: datetime, date_fin: datetime) -> str:
+def _construire_filtre_date(cle_date: str, date_inferieure: datetime, date_superieure: datetime) -> str:
     """
     Construit un filtre par rapport à un intervalle de temps.
 
     Parameters:
         cle_date (str): La clé JSON de la valeur de la date.
-        date_limite (datetime): La borne inférieure de l'intervalle de temps (exclue).
-        date_fin (datetime): La borne supérieure de l'intervalle de temps (incluse).
+        date_inferieure (datetime): La borne inférieure de l'intervalle de temps (exclue).
+        date_superieure (datetime): La borne supérieure de l'intervalle de temps (incluse).
 
     Returns:
         (str): Le filtre de l'intervalle de temps.
     """
 
-    nb_heures = int((date_fin - date_limite).total_seconds() / 3600)
-
-    # Si le nombre d'heures est supérieur à 6
-    if nb_heures > 6:
-        nb_heures = 6
-        # Avancer la date de début à la date de fin moins 6 heures
-        date_limite = date_fin - timedelta(hours=6)
+    nb_heures = int((date_superieure - date_inferieure).total_seconds() / 3600)
 
     conditions_date = []
     # La date de début n'est pas incluse dans l'itération
     for i in range(nb_heures):
-        date_iteration = date_fin - timedelta(hours=i)
+        date_iteration = date_superieure - timedelta(hours=i)
 
         conditions_date.extend(
             [
@@ -85,67 +79,79 @@ def extraire_releves(app: Flask) -> dict[str, list[dict]]:
         )
 
         with app.app_context():
-            derniere_date = (
+            date_dernier_releve = (
                 db.session.query(db.func.max(Releve.date)).filter(Releve.type_releve == type_releve).scalar()
             )
 
-        if derniere_date:
-            derniere_date = derniere_date.replace(tzinfo=FUSEAU_HORAIRE)
-        else:
-            derniere_date = date - timedelta(hours=6)
+        if not date_dernier_releve:
+            date_dernier_releve = date - timedelta(hours=24)
 
-        if jeu_donnees == JeuxDonnees.HYDROMETEOROLOGIQUES:
-            filtre_date = _construire_filtre_date("date", derniere_date, date)
-        elif jeu_donnees == JeuxDonnees.HYDROMETRIQUES:
-            filtre_date = _construire_filtre_date("split_date", derniere_date, date)
+        date_dernier_releve = date_dernier_releve.replace(tzinfo=FUSEAU_HORAIRE)
 
-        # S'il n'y a pas de filtre par rapport à la date, alors tous les relevés sont à jour en base de données
-        if not filtre_date:
-            continue
-
-        url_api = f"{constantes.URL_API_DONNEES_HQ}/{jeu_donnees.value}/records{QUERY_PAR_DEFAUT}&where={urllib.parse.quote(filtre_date)}"  # noqa: E501
+        nb_heures_retard = int((date - date_dernier_releve).total_seconds() / 3600)
+        if nb_heures_retard > 24:
+            nb_heures_retard = 24
+            date_dernier_releve = date - timedelta(hours=24)
 
         releves_jeu_donnees = []
-        decalage = 0
-        total_elements = float("inf")  # Utiliser "inf" pour forcer la première itération
 
-        print(f"Extraction des données dans l'intervalle de temps : ]{derniere_date.isoformat()}; {date.isoformat()}].")
+        for i in range(0, nb_heures_retard, 3):
+            nb_heures_intervalle = min(3, nb_heures_retard - i)
 
-        while decalage < total_elements:
-            print(f"Extraction des données du jeu {jeu_donnees.value} (decalage={decalage})")
+            date_debut_intervalle = date_dernier_releve + timedelta(hours=i)
+            date_fin_intervalle = date_debut_intervalle + timedelta(hours=nb_heures_intervalle)
 
-            url_api_paginee = f"{url_api}&offset={decalage}"
+            cle_date = "date" if jeu_donnees == JeuxDonnees.HYDROMETEOROLOGIQUES else "split_date"
 
-            try:
-                reponse = session.get(url_api_paginee, timeout=constantes.TIMEOUT_REQUETE_SECONDES)
-                reponse.encoding = "UTF-8"
-                reponse.raise_for_status()
+            filtre_date = _construire_filtre_date(
+                cle_date,
+                date_inferieure=date_debut_intervalle,
+                date_superieure=date_fin_intervalle,
+            )
 
-                corps_reponse = reponse.json()
+            url_api = f"{constantes.URL_API_DONNEES_HQ}/{jeu_donnees.value}/records{QUERY_PAR_DEFAUT}&where={urllib.parse.quote(filtre_date)}"  # noqa: E501
 
-                # Mettre à jour le nombre total de relevés à extraire lors de première itération
-                if decalage == 0:
-                    total_elements = corps_reponse.get("total_count", 0)
+            decalage = 0
+            total_elements = float("inf")  # Utiliser "inf" pour forcer la première itération
 
-                releves_jeu_donnees.extend(corps_reponse.get("results", []))
+            print(
+                "Extraction des données dans l'intervalle de temps :"
+                + f"]{date_debut_intervalle.isoformat()}; {date_fin_intervalle.isoformat()}]"
+            )
 
-                decalage += constantes.LIMITE_ELEMENTS_API
+            while decalage < total_elements:
+                print(f"Extraction des données du jeu {jeu_donnees.value} (decalage={decalage})")
 
-                # Si une autre itération doit être faite, attendre 3 secondes
-                if decalage < total_elements:
-                    time.sleep(constantes.PAUSE_ENTRE_REQUETES_SECONDES)
+                url_api_paginee = f"{url_api}&offset={decalage}"
 
-            except requests.exceptions.RequestException:
-                print(
-                    f"Échec de la récupération des relevés d'Hydro-Québec pour le jeu {jeu_donnees.value} (decalage={decalage})."  # noqa: E501
-                )
-                # Arrêter l'extraction des relevés pour le jeu de l'itération
-                break
+                try:
+                    reponse = session.get(url_api_paginee, timeout=constantes.TIMEOUT_REQUETE_SECONDES)
+                    reponse.encoding = "UTF-8"
+                    reponse.raise_for_status()
+
+                    corps_reponse = reponse.json()
+
+                    # Mettre à jour le nombre total de relevés à extraire lors de première itération
+                    if decalage == 0:
+                        total_elements = corps_reponse.get("total_count", 0)
+
+                    releves_jeu_donnees.extend(corps_reponse.get("results", []))
+
+                    decalage += constantes.LIMITE_ELEMENTS_API
+
+                    # Si une autre itération doit être faite, attendre 3 secondes
+                    if decalage < total_elements:
+                        time.sleep(constantes.PAUSE_ENTRE_REQUETES_SECONDES)
+
+                except requests.exceptions.RequestException:
+                    print(
+                        f"Échec de la récupération des relevés d'Hydro-Québec pour le jeu {jeu_donnees.value} (decalage={decalage})."  # noqa: E501
+                    )
+                    # Arrêter l'extraction des relevés pour le jeu de l'itération
+                    break
 
         print(f"{len(releves_jeu_donnees)} relevés extraits du jeu {jeu_donnees.value}.")
 
         releves_extraits[jeu_donnees.value] = releves_jeu_donnees
-
-        time.sleep(constantes.PAUSE_ENTRE_REQUETES_SECONDES)
 
     return releves_extraits
