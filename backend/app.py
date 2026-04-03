ab1e3220067b9ba6db9cf9a27e868624c787fb6f
Arhusien <87914117+Arhusien@users.noreply.github.com>
2026-04-02 22:20:14 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,9 @@ from routes import bp_carte
 from services.synchronisation_service import associer_sondes_centrales, synchroniser_installations
 
 app = Flask(__name__)
-
 app.config.from_object("config.Config")
-db.init_app(app)
 
+db.init_app(app)
 cache.init_app(app)
 
 app.register_blueprint(bp_carte)
