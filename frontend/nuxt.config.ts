0307bdffcf07920031b3115e5e12759c427aa29e
Arhusien <87914117+Arhusien@users.noreply.github.com>
2026-04-15 09:17:33 -0400

    Ajout favicones

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,40 @@
 import tailwindcss from "@tailwindcss/vite";
 
+const faviconPaths = [
+    "android-chrome-192x192.png",
+    "android-chrome-512x512.png",
+    "apple-touch-icon.png",
+    "favicon-16x16.png",
+    "favicon-32x32.png",
+    "favicon.ico",
+];
+
+const faviconLinks = faviconPaths.flatMap((name) => {
+    const isIco = name.endsWith(".ico"),
+        type = isIco ? "image/x-icon" : "image/png",
+        rel = name.includes("apple-touch-icon") ? "apple-touch-icon" : "icon",
+        size = isIco ? undefined : name.match(/(\d+x\d+)/)?.[0];
+
+    const baseLink = {
+        rel,
+        type,
+        size,
+    };
+
+    return [
+        {
+            ...baseLink,
+            href: `/favicon/dark/${name}`,
+            media: "(prefers-color-scheme: light)",
+        },
+        {
+            ...baseLink,
+            href: `/favicon/light/${name}`,
+            media: "(prefers-color-scheme: dark)",
+        },
+    ];
+});
+
 export default defineNuxtConfig({
     modules: [
         "@nuxt/eslint",
@@ -15,6 +50,15 @@ export default defineNuxtConfig({
     ssr: false,
     devtools: {
         enabled: true,
+    },
+    app: {
+        head: {
+            title: "Atlas Hydro",
+            link: [
+                ...faviconLinks,
+            ],
+        },
+
     },
     css: [
         "./app/assets/css/main.css",
